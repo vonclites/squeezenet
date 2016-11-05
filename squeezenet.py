27796a51f9a0a62d14df741cb49f7f2de9c594e5
vonclites <alamos52@gmail.com>
2016-11-05 17:37:36 -0400

    Removed specialized code to force device placement.  Apparently was unnecessary.

--- squeezenet.py
+++ squeezenet.py
@@ -38,10 +38,10 @@ def expand(inputs, num_outputs):
 def inference(images):
     with slim.arg_scope(squeezenet_arg_scope()):
         with tf.variable_scope('squeezenet', values=[images]) as sc:
-            end_points_collection = sc.original_name_scope + '_end_points'
+            end_point_collection = sc.original_name_scope + '_end_points'
             with slim.arg_scope([fire_module, slim.conv2d,
                                  slim.max_pool2d, slim.avg_pool2d],
-                                 outputs_collections=[end_points_collection]):
+                                outputs_collections=[end_point_collection]):
                 net = slim.conv2d(images, 96, [2, 2], scope='conv1')
                 net = slim.max_pool2d(net, [2, 2], scope='maxpool1')
                 net = fire_module(net, 16, 64, scope='fire2')
@@ -54,21 +54,20 @@ def inference(images):
                 net = fire_module(net, 64, 256, scope='fire8')
                 net = slim.max_pool2d(net, [2, 2], scope='maxpool8')
                 net = fire_module(net, 64, 256, scope='fire9')
-                # Reversed avg and conv layers per NiN
+                # Reversed avg and conv layers per 'Network in Network'
                 net = slim.avg_pool2d(net, [4, 4], scope='avgpool10')
                 net = slim.conv2d(net, 10, [1, 1],
                                   activation_fn=None,
                                   normalizer_fn=None,
                                   scope='conv10')
                 logits = tf.squeeze(net, [1, 2], name='logits')
-                logits = utils.collect_named_outputs(end_points_collection,
+                logits = utils.collect_named_outputs(end_point_collection,
                                                      sc.name + '/logits',
                                                      logits)
-            end_points = utils.convert_collection_to_dict(end_points_collection)
+            end_points = utils.convert_collection_to_dict(end_point_collection)
     return logits, end_points
 
 
 def squeezenet_arg_scope():
-    with slim.arg_scope([slim.conv2d], normalizer_fn=slim.batch_norm,
-                        device='/device:CPU:0') as sc:
+    with slim.arg_scope([slim.conv2d], normalizer_fn=slim.batch_norm) as sc:
         return sc
