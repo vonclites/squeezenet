c189b584af960483bc99920633e146e65a3ca726
Domenick Poster <alamos52@gmail.com>
2017-04-11 14:45:57 -0400

    Worked on separating cifar squeezenet from original version

--- squeezenet.py
+++ squeezenet.py
@@ -35,9 +35,44 @@ def expand(inputs, num_outputs):
     return tf.concat(3, [e1x1, e3x3])
 
 
-def inference(images, 
-              is_training=True,
-              batch_norm_decay=0.999):
+def squeezenet(images,
+               is_training=True,
+               batch_norm_decay=0.999,
+               num_classes=1000):
+    """Original squeezenet architecture for 224x224 images."""
+    with slim.arg_scope(squeezenet_arg_scope(is_training, batch_norm_decay)):
+        with tf.variable_scope('squeezenet', values=[images]) as sc:
+            end_point_collection = sc.original_name_scope + '_end_points'
+            with slim.arg_scope([fire_module, slim.conv2d,
+                                 slim.max_pool2d, slim.avg_pool2d],
+                                outputs_collections=[end_point_collection]):
+                net = slim.conv2d(images, 96, [7, 7], stride=2, scope='conv1')
+                net = slim.max_pool2d(net, [3, 3], stride=2, scope='maxpool1')
+                net = fire_module(net, 16, 64, scope='fire2')
+                net = fire_module(net, 16, 64, scope='fire3')
+                net = fire_module(net, 32, 128, scope='fire4')
+                net = slim.max_pool2d(net, [3, 3], stride=2, scope='maxpool4')
+                net = fire_module(net, 32, 128, scope='fire5')
+                net = fire_module(net, 48, 192, scope='fire6')
+                net = fire_module(net, 48, 192, scope='fire7')
+                net = fire_module(net, 64, 256, scope='fire8')
+                net = slim.max_pool2d(net, [3, 3], stride=2, scope='maxpool8')
+                net = fire_module(net, 64, 256, scope='fire9')
+                net = slim.dropout(net, is_training=is_training, scope='drop9')
+                net = slim.conv2d(net, num_classes, [1, 1], stride=1, scope='conv10')
+                net = slim.avg_pool2d(net, [13, 13], stride=1, scope='avgpool10')
+                logits = tf.squeeze(net, [1, 2], name='logits')
+                logits = utils.collect_named_outputs(end_point_collection,
+                                                     sc.name + '/logits',
+                                                     logits)
+            end_points = utils.convert_collection_to_dict(end_point_collection)
+            return logits, end_points
+
+def cifar_squeezenet(images,
+                     is_training=True,
+                     batch_norm_decay=0.999,
+                     num_classes=10):
+    """Modified version of squeezenet for CIFAR images"""
     with slim.arg_scope(squeezenet_arg_scope(is_training, batch_norm_decay)):
         with tf.variable_scope('squeezenet', values=[images]) as sc:
             end_point_collection = sc.original_name_scope + '_end_points'
@@ -56,9 +91,9 @@ def inference(images,
                 net = fire_module(net, 64, 256, scope='fire8')
                 net = slim.max_pool2d(net, [2, 2], scope='maxpool8')
                 net = fire_module(net, 64, 256, scope='fire9')
-                # Reversed avg and conv layers per 'Network in Network'
+                # Use global average pooling per 'Network in Network [1]'
                 net = slim.avg_pool2d(net, [4, 4], scope='avgpool10')
-                net = slim.conv2d(net, 10, [1, 1],
+                net = slim.conv2d(net, num_classes, [1, 1],
                                   activation_fn=None,
                                   normalizer_fn=None,
                                   scope='conv10')
@@ -76,3 +111,9 @@ def squeezenet_arg_scope(is_training, decay):
                           is_training=is_training,
                           decay=decay) as sc:
         return sc
+
+
+'''
+Network in Network: https://arxiv.org/abs/1312.4400
+See Section 3.2 for global average pooling
+'''
